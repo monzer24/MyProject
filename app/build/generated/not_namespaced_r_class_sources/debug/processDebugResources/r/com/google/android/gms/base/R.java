e68f29ac38870fca5839b599b8e74dcf17bf5845
Monzer <monzer.husaini@gmail.com>
2019-04-01 11:18:45 +0300

    test

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -65,12 +65,12 @@ public final class R {
         public static final int adjust_height = 0x7f07001b;
         public static final int adjust_width = 0x7f07001c;
         public static final int auto = 0x7f070021;
-        public static final int dark = 0x7f070035;
-        public static final int icon_only = 0x7f07004b;
-        public static final int light = 0x7f070052;
-        public static final int none = 0x7f07005c;
-        public static final int standard = 0x7f070085;
-        public static final int wide = 0x7f07009d;
+        public static final int dark = 0x7f070036;
+        public static final int icon_only = 0x7f07004c;
+        public static final int light = 0x7f070053;
+        public static final int none = 0x7f07005d;
+        public static final int standard = 0x7f070086;
+        public static final int wide = 0x7f07009e;
     }
     public static final class string {
         private string() {}
--- app/src/main/java/com/projects/check/MainActivity.java
+++ app/src/main/java/com/projects/check/MainActivity.java
@@ -2,15 +2,22 @@ package com.projects.check;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
@@ -19,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
     SurfaceView cameraView;
     TextView textView;
     CameraSource cameraSource;
+    Button capture;
+    static SparseArray<TextBlock> items; // to save the detected text
+    private final int RequestPermissionID =1001;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case RequestPermissionID:{
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +54,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cameraView = (SurfaceView) findViewById(R.id.surface_view);
         textView = (TextView) findViewById(R.id.text_view);
+        capture = findViewById(R.id.button);
         TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
         if (!textRecognizer.isOperational()) {
 //            log.w("MainActivity", "Detector dependencies are not yet available");
             Log.w("MainActivity", "onCreate: Detector dependencies are not yet available");
         } else {
-            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer).setRequestedPreviewSize(1280, 786)
+                    .setRequestedFps(15)
                     .setFacing(CameraSource.CAMERA_FACING_BACK)
                     .setAutoFocusEnabled(true)
                     .build();
@@ -41,17 +71,12 @@ public class MainActivity extends AppCompatActivity {
                     try {
 
                         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                            // TODO: Consider calling
-                            //    ActivityCompat#requestPermissions
-                            // here to request the missing permissions, and then overriding
-                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                            //                                          int[] grantResults)
-                            // to handle the case where the user grants the permission. See the documentation
-                            // for ActivityCompat#requestPermissions for more details.
-                            cameraSource.start(cameraView.getHolder());
-
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[] {Manifest.permission.CAMERA},
+                                    RequestPermissionID);
                             return;
                         }
+                        cameraSource.start(cameraView.getHolder());
 
                     }
                     catch (IOException e)
@@ -67,7 +92,37 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void surfaceDestroyed(SurfaceHolder holder) {
+                    cameraSource.stop();
+                }
+            });
 
+
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+                // = detections.getDetectedItems();
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    items = detections.getDetectedItems();
+
+                }
+            });
+
+            capture.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    for(int i = 0; i < items.size(); i++){
+                        if(items.get(i).getValue() != null){
+                            textView.setText(items.get(i).getValue());
+                        }else{
+                            textView.setText("Capture Again");
+                            textView.setTextColor(Color.red(1));
+                        }
+                    }
                 }
             });
 

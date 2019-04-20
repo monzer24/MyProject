f919225e86eb8e3ffa65835b372615aad77b729a
monzer24 <monzer.husaini@gmail.com>
2019-04-20 15:58:47 +0300

    Fixed Signup failure

---
+++ checkeradmin/src/main/java/com/example/checkeradmin/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.checkeradmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText accountNo;
+    EditText password;
+    Button login;
+    private FirebaseConnection connection;
+    private View.OnClickListener logIn = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        connection = new FirebaseConnection(this);
+
+
+        accountNo = findViewById(R.id.bank_no);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(logIn);
+    }
+}

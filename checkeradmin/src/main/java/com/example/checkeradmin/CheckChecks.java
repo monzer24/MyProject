8ea3b59c305bcecc947beafae9ff73cab4544b2c
monzer24 <monzer.husaini@gmail.com>
2019-04-21 10:34:23 +0300

    Fixed Signup failure

--- checkeradmin/src/main/java/com/example/checkeradmin/CheckChecks.java
+++
@@ -1,6 +0,0 @@
-package com.example.checkeradmin;
-
-import android.app.Activity;
-
-public class CheckChecks extends Activity {
-}
--- checkeradmin/src/main/java/com/example/checkeradmin/Connection.java
+++
@@ -1,7 +0,0 @@
-package com.example.checkeradmin;
-
-public interface Connection {
-
-    void logIn(User user);
-
-}
